a30f6b36e5cd6cbd588de12b3adb3bad33b98a06
othniel <ottneel@gmail.com>
2024-02-16 11:12:28 -0800

    added changes to the pipeline script

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.9
+
+# Set working directory
+WORKDIR /app
+
+# Update package index and install wget
+RUN apt-get update && apt-get install -y wget
+
+# Install Python dependencies
+RUN pip install pandas sqlalchemy
+
+# Copy the Python script into the container
+COPY green_taxi_pipeline.py .
+
+# Set the entry point for the container
+ENTRYPOINT [ "python", "green_taxi_pipeline.py" ]
--- green_taxi_pipeline.py
+++ green_taxi_pipeline.py
@@ -1,85 +1,90 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
-
-
 #import necessary libraries
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
 import subprocess
+import argparse
+
+#define the main function which takes in the parameters to be used.
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    table_name2=params.table_name2
+    url2 = params.url2
+    url = params.url
 
 
-# In[ ]:
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    subprocess.run([r"C:\Program Files\Git\mingw64\bin\wget.exe", url, "-O", csv_name])
+
+    if url2.endswith('.csv.gz'):
+        csv2_name = 'output2.csv.gz'
+    else:
+        csv2_name = 'output2.csv'
+
+    subprocess.run([r"C:\Program Files\Git\mingw64\bin\wget.exe", url2, "-O", csv2_name])
 
 
+    #create the connection engine to the database
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    #check the database connection
+    engine.connect()
 
 
+    # create a dataframe iterator to load the data in chunk sizes
+    data_iter = pd.read_csv(csv_name, iterator = True, chunksize = 100000)
 
-# In[ ]:
+    #input the table names in the database using the to_sql method
+    data.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-
-if url.endswith('.csv.gz'):
-    csv_name = 'output.csv.gz'
-else:
-    csv_name = 'output.csv'
-
-subprocess.run(["wget", url, "-O", csv_name])
-
-
-# In[22]:
-
-
-#create the connection engine to the database
-engine = create_engine('postgresql://roots:roots@localhost:5431/ny_taxi')
-#check the database connection
-engine.connect()
-
-
-# In[25]:
-
-
-# create a dataframe iterator to load the data in chunk sizes
-data_iter = pd.read_csv('green_tripdata_2019-09.csv', iterator = True, chunksize = 100000)
-
-
-# In[24]:
-
-
-#input the table names in the database using the to_sql method
-data.head(0).to_sql(name = 'green_taxi_data', con = engine, if_exists='replace')
-
-
-# In[26]:
-
-
-#try block to iterate through the dataset and load the data chunk by chunk. and stop when there are no more chunks to add
-try:
+    #try block to iterate through the dataset and load the data chunk by chunk. and stop when there are no more chunks to add
     while True:
-        t_start = time() #track start time of each chunk
-        data = next(data_iter)
-        #change the datatype of the pickup and dropoff time from object to datetime
-        data['lpep_pickup_datetime'] = pd.to_datetime(data['lpep_pickup_datetime'])
-        data['lpep_dropoff_datetime'] = pd.to_datetime(data['lpep_dropoff_datetime'])
-        data.to_sql(name = 'green_taxi_data', con = engine, if_exists='append')
+        try:
+            t_start = time() #track start time of each chunk
+            data = next(data_iter)
+            #change the datatype of the pickup and dropoff time from object to datetime
+            data['lpep_pickup_datetime'] = pd.to_datetime(data['lpep_pickup_datetime'])
+            data['lpep_dropoff_datetime'] = pd.to_datetime(data['lpep_dropoff_datetime'])
+            data.to_sql(name = table_name, con = engine, if_exists='append')
     
-        t_end = time() #track end time of each chunk
+            t_end = time() #track end time of each chunk
     
-        print('inserted another chunk...., took %.3f second' % (t_end - t_start))
-except StopIteration:
-    print('No more chunks to add')
+            print('inserted another chunk...., took %.3f second' % (t_end - t_start))
+        except StopIteration:
+            print('No more chunks to add')
+        break
+
+    #read and load the second table in the database using the to_sql method
+    data2=pd.read_csv(csv2_name)
+    data2.to_sql(name = table_name2, con = engine, if_exists='replace')
 
 
-# In[30]:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--table_name2', required=True, help='name of the second table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--url2', required=True, help='url of the second csv file')
 
-#input the second table in the database using the to_sql method
-data2.to_sql(name = 'taxi+_zone_lookup', con = engine, if_exists='replace')
+    args = parser.parse_args()
 
-
-# In[ ]:
-
-
+    main(args)
 
 
